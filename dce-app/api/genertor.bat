3323b49f9ac588e525e29c6c4726829697bf09e4
baishui2015 <liu83sihai@163.com>
2018-12-01 19:25:29 +0800

    gec

--- dce-app/api/genertor.bat
+++
@@ -1,2 +0,0 @@
-apidoc -i D:/codes/travel/code/travel/dce-app/src/main/java/com/dce/business/ -o D:/codes/travel/code/travel/dce-app/src/main/webapp/apidoc/ -t D:/codes/travel/code/travel/dce-app/api/mytemplate/
-pause
